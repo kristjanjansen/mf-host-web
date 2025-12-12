1f066edf2892937b067a30e4d7f984d7e248b03f
Kristjan Jansen <Kristjan.Jansen@enefit.ee>
2025-12-12 08:51:15 +0200

    Bring back files

---
+++ src/components/MfRoute.tsx
@@ -0,0 +1,20 @@
+import type { MfConfig } from "../config/config";
+import { useMfScript } from "../utils/utils";
+
+export function MfRoute({ mf, slot }: { mf: MfConfig; slot?: string }) {
+  const ready = useMfScript(mf);
+  const label = mf.label ?? mf.id;
+
+  if (!ready) {
+    return (
+      <div slot={slot}>
+        <h2>{label}</h2>
+        <div>Loading {label}</div>
+      </div>
+    );
+  }
+
+  const Tag = mf.tag as any;
+
+  return <Tag slot={slot} />;
+}
